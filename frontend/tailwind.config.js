7dd3d1e85d0503be5b27ba1891254d3de786982b
Varun Sharma <galvevarun@gmail.com>
2025-11-27 01:58:16 +0530

    Fixes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './ui-backup/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        mono: ['var(--font-jetbrains-mono)'],
+      },
+    },
+  },
+  plugins: [],
+}
